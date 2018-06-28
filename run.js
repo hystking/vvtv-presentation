627811156441e692fece92dda905031125e7460d
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2018-06-28 17:13:18 +0900

    misc

--- run.js
+++ run.js
@@ -2,12 +2,12 @@ const template = require("./template.json");
 const fs = require("fs");
 
 
-const files = fs.readdirSync(".");
+const files = fs.readdirSync("img");
 
 template.whiteboard.urls =
   files
   .filter(name => /\.png$/.test(name))
   .sort((a, b) => a < b ? -1 : 1)
-  .map(name => `https://hystking.github.io/vvtv-presentation/${name}`);
+  .map(name => `https://hystking.github.io/vvtv-presentation/img/${name}`);
 
 console.log(JSON.stringify(template, null, 2));
