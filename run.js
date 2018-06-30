c5845dc43e90349ea98f3eaa9b6aed27fde562a7
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2018-06-30 18:20:40 +0900

    reverse order

--- run.js
+++ run.js
@@ -7,7 +7,7 @@ const files = fs.readdirSync("img");
 template.whiteboard.urls =
   files
   .filter(name => /\.png$/.test(name))
-  .sort((a, b) => a < b ? -1 : 1)
+  .sort((a, b) => a < b ? 1 : -1)
   .map(name => `https://hystking.github.io/vvtv-presentation/img/${name}`);
 
 console.log(JSON.stringify(template, null, 2));
